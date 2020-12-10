17a0d104d057863b9e399c4e6e1df8850608469d
Bartłomiej Fryz <fryzbartlomiej@gmail.com>
2020-12-10 20:41:17 +0530

    Fix useNativeDriver explicity

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -235,7 +235,7 @@ export default class TextField extends PureComponent {
 
     let options = {
       toValue: this.labelState(),
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration,
     };
 
